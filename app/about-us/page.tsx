f5e105dd09e207bc2de82ed792c70c52c674390e
jueun_choi <jueun_choi2@tmax.co.kr>
2024-03-12 17:07:26 +0900

    feat: route 사용

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -1,7 +1,12 @@
-export default function AboutUs() {
+export default async function AboutUs() {
+  const test = await fetch("http://localhost:3000/api/about");
+
+  const json = await test.json();
+
   return (
     <div>
       <h1>about us...</h1>
+      <div>{JSON.stringify(json)}</div>
     </div>
   );
 }
