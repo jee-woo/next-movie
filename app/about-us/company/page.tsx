88a6ce580dbbe81dd3f7feb1e8c82989c4ec7824
jueun_choi <jueun_choi2@tmax.co.kr>
2024-03-13 20:41:45 +0900

    comment: form action 설명 추가

--- app/about-us/company/page.tsx
+++ app/about-us/company/page.tsx
@@ -3,6 +3,7 @@ export default function Company() {
     "use server";
     // 서버에 바로 접근하는 코드
     // 빌드 시점에 미리 클라이언트에서 분리시키고 서버로 옮김으로써 클라이언트 번들링 결과물에는 포함되지 않고 서버에서만 실행도히는 서버 액션 만듦
+    // 서버 컴포넌트에서는 use server가 없으면 form의 action을 사용할 수 없음
 
     console.log(
       "해당 작업은 서버에서 수행합니다... 따라서 CORS 이슈가 없습니다..."
